0be0d14fb3213d9d21a05463b71ae7e05292ef82
Vitaliy <vitohatv@gmail.com>
2023-09-21 02:18:54 +0600

    Добавил главную страницу с описанием

--- deencode.js
+++ deencode.js
@@ -4,7 +4,12 @@ const port    = 1605;
 
 const Caesar = require('caesar-salad').Caesar;
 
-
+app.get('/', (request, response) => {
+    response.send(`Главная страница. Для использования шифратора необходимо
+    переидти по пути: /encode/слово которое необходимо зашифорвать и так же
+    дешифратор: /decode/зашифрованное слово`);
+    request.end();
+});
 
 app.listen(port);
 console.log('Server running on port 1605: http://localhost:' + port);
